1529179816f7f7120a79f7f52d68c35add1eebbf
sri lakshman <147168852+srilakshman73@users.noreply.github.com>
2026-08-22 11:55:09 +0530

    Refactor robot control code and add modes

--- All_in_One_Robot.ino
+++ All_in_One_Robot.ino
@@ -1,230 +1,348 @@
-#include <SoftwareSerial.h>
-SoftwareSerial BT_Serial(2, 3); // RX, TX
+#include <Servo.h>
+#include <NewPing.h>
 
-#include <IRremote.h>
-const int RECV_PIN = A5;
-IRrecv irrecv(RECV_PIN);
-decode_results results;
+#define OBSTACLE_AVOIDING_MODE                    1
+#define LINE_FOLLOWER_MODE                        2
+#define LINE_FOLLOWER_WITH_OBSTACLE_AVOIDING_MODE 3
+#define OBJECT_FOLLOWING_MODE                     4
+#define ROBOT_CONTROL_MODE OBSTACLE_AVOIDING_MODE
+#define SERVO_PIN 3
+#define ULTRASONIC_SENSOR_TRIG 11
+#define ULTRASONIC_SENSOR_ECHO 12
 
-#define enA 10//Enable1 L298 Pin enA 
-#define in1 9 //Motor1  L298 Pin in1 
-#define in2 8 //Motor1  L298 Pin in1 
-#define in3 7 //Motor2  L298 Pin in1 
-#define in4 6 //Motor2  L298 Pin in1 
-#define enB 5 //Enable2 L298 Pin enB 
+#define BOTTOM_IR_SENSOR_RIGHT A0
+#define BOTTOM_IR_SENSOR_LEFT A1
+#define TOP_IR_SENSOR_RIGHT A3
+#define TOP_IR_SENSOR_LEFT A2
 
-#define servo A4
 
-#define R_S A0 //ir sensor Right
-#define L_S A1 //ir sensor Left
+//Right motor
+int enableRightMotor=5;
+int rightMotorPin1=7;
+int rightMotorPin2=8;
 
-#define echo A2    //Echo pin
-#define trigger A3 //Trigger pin
+//Left motor
+int enableLeftMotor=6;
+int leftMotorPin1=9;
+int leftMotorPin2=10;
 
-int distance_L, distance_F = 30, distance_R;
-long distance;
-int set = 20;
+NewPing mySensor(ULTRASONIC_SENSOR_TRIG, ULTRASONIC_SENSOR_ECHO, 400);
+Servo myServo;
 
-int bt_ir_data; // variable to receive data from the serial port and IRremote
-int Speed = 130;  
-int mode=0;
-int IR_data;
+void setup()
+{
+  // put your setup code here, to run once:
+  pinMode(enableRightMotor,OUTPUT);
+  pinMode(rightMotorPin1,OUTPUT);
+  pinMode(rightMotorPin2,OUTPUT);
+  
+  pinMode(enableLeftMotor,OUTPUT);
+  pinMode(leftMotorPin1,OUTPUT);
+  pinMode(leftMotorPin2,OUTPUT);
 
-void setup(){ // put your setup code here, to run once
+  myServo.attach(SERVO_PIN);
+  myServo.write(90);
 
-pinMode(R_S, INPUT); // declare if sensor as input  
-pinMode(L_S, INPUT); // declare ir sensor as input
+  pinMode(BOTTOM_IR_SENSOR_RIGHT, INPUT);
+  pinMode(BOTTOM_IR_SENSOR_LEFT, INPUT);
 
-pinMode(echo, INPUT );// declare ultrasonic sensor Echo pin as input
-pinMode(trigger, OUTPUT); // declare ultrasonic sensor Trigger pin as Output  
-
-pinMode(enA, OUTPUT); // declare as output for L298 Pin enA 
-pinMode(in1, OUTPUT); // declare as output for L298 Pin in1 
-pinMode(in2, OUTPUT); // declare as output for L298 Pin in2 
-pinMode(in3, OUTPUT); // declare as output for L298 Pin in3   
-pinMode(in4, OUTPUT); // declare as output for L298 Pin in4 
-pinMode(enB, OUTPUT); // declare as output for L298 Pin enB 
-
-irrecv.enableIRIn(); // Start the receiver
-irrecv.blink13(true);
-
-Serial.begin(9600); // start serial communication at 9600bps
-BT_Serial.begin(9600); 
-
-pinMode(servo, OUTPUT);
-
- for (int angle = 70; angle <= 140; angle += 5)  {
-   servoPulse(servo, angle);  }
- for (int angle = 140; angle >= 0; angle -= 5)  {
-   servoPulse(servo, angle);  }
-
- for (int angle = 0; angle <= 70; angle += 5)  {
-   servoPulse(servo, angle);  }
-delay(500);
+  pinMode(TOP_IR_SENSOR_RIGHT, INPUT);
+  pinMode(TOP_IR_SENSOR_LEFT, INPUT);
+    
+  rotateMotor(0,0);   
 }
 
+void obstacleAvoidingControl()
+{
+  static int MAX_REGULAR_MOTOR_SPEED  = 150;
+  static int MAX_MOTOR_TURN_SPEED  = 200;
+  static int DISTANCE_TO_CHECK  = 30;
+  
+  int distance = mySensor.ping_cm();
 
-void loop(){  
-
-if(BT_Serial.available() > 0){  //if some date is sent, reads it and saves in state     
-bt_ir_data = BT_Serial.read(); 
-Serial.println(bt_ir_data);     
-if(bt_ir_data > 20){Speed = bt_ir_data;}      
-}
-
-if (irrecv.decode(&results)) {
-Serial.println(results.value,HEX);
-bt_ir_data = IRremote_data();
-Serial.println(bt_ir_data); 
-irrecv.resume(); // Receive the next value
-delay(100);
-}
-
-     if(bt_ir_data == 8){mode=0; Stop();}    //Manual Android Application and IR Remote Control Command   
-else if(bt_ir_data == 9){mode=1; Speed=130;} //Auto Line Follower Command
-else if(bt_ir_data ==10){mode=2; Speed=255;} //Auto Obstacle Avoiding Command
-
-analogWrite(enA, Speed); // Write The Duty Cycle 0 to 255 Enable Pin A for Motor1 Speed 
-analogWrite(enB, Speed); // Write The Duty Cycle 0 to 255 Enable Pin B for Motor2 Speed 
-
-if(mode==0){     
-//===============================================================================
-//                          Key Control Command
-//=============================================================================== 
-     if(bt_ir_data == 1){forword(); }  // if the bt_data is '1' the DC motor will go forward
-else if(bt_ir_data == 2){backword();}  // if the bt_data is '2' the motor will Reverse
-else if(bt_ir_data == 3){turnLeft();}  // if the bt_data is '3' the motor will turn left
-else if(bt_ir_data == 4){turnRight();} // if the bt_data is '4' the motor will turn right
-else if(bt_ir_data == 5){Stop(); }     // if the bt_data '5' the motor will Stop
-
-//===============================================================================
-//                          Voice Control Command
-//===============================================================================    
-else if(bt_ir_data == 6){turnLeft();  delay(400);  bt_ir_data = 5;}
-else if(bt_ir_data == 7){turnRight(); delay(400);  bt_ir_data = 5;}
-}
-
-if(mode==1){    
-//===============================================================================
-//                          Line Follower Control
-//===============================================================================     
-if((digitalRead(R_S) == 0)&&(digitalRead(L_S) == 0)){forword();}  //if Right Sensor and Left Sensor are at White color then it will call forword function
-if((digitalRead(R_S) == 1)&&(digitalRead(L_S) == 0)){turnRight();}//if Right Sensor is Black and Left Sensor is White then it will call turn Right function  
-if((digitalRead(R_S) == 0)&&(digitalRead(L_S) == 1)){turnLeft();} //if Right Sensor is White and Left Sensor is Black then it will call turn Left function
-if((digitalRead(R_S) == 1)&&(digitalRead(L_S) == 1)){Stop();}     //if Right Sensor and Left Sensor are at Black color then it will call Stop function
-} 
-
-if(mode==2){    
-//===============================================================================
-//                          Obstacle Avoiding Control
-//===============================================================================     
- distance_F = Ultrasonic_read();
- Serial.print("S=");Serial.println(distance_F);
-  if (distance_F > set){forword();}
-    else{Check_side();}
-}
-
-delay(10);
-}
-
-long IRremote_data(){
-     if(results.value==0xFF02FD){IR_data=1;}  
-else if(results.value==0xFF9867){IR_data=2;} 
-else if(results.value==0xFFE01F){IR_data=3;} 
-else if(results.value==0xFF906F){IR_data=4;} 
-else if(results.value==0xFF629D || results.value==0xFFA857){IR_data=5;} 
-else if(results.value==0xFF30CF){IR_data=8;} 
-else if(results.value==0xFF18E7){IR_data=9;} 
-else if(results.value==0xFF7A85){IR_data=10;} 
-return IR_data;
-}
-
-void servoPulse (int pin, int angle){
-int pwm = (angle*11) + 500;      // Convert angle to microseconds
- digitalWrite(pin, HIGH);
- delayMicroseconds(pwm);
- digitalWrite(pin, LOW);
- delay(50);                   // Refresh cycle of servo
-}
-
-
-//**********************Ultrasonic_read****************************
-long Ultrasonic_read(){
-  digitalWrite(trigger, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigger, HIGH);
-  delayMicroseconds(10);
-  distance = pulseIn (echo, HIGH);
-  return distance / 29 / 2;
-}
-
-void compareDistance(){
-       if (distance_L > distance_R){
-  turnLeft();
-  delay(350);
-  }
-  else if (distance_R > distance_L){
-  turnRight();
-  delay(350);
-  }
-  else{
-  backword();
-  delay(300);
-  turnRight();
-  delay(600);
-  }
-}
-
-void Check_side(){
-    Stop();
-    delay(100);
- for (int angle = 70; angle <= 140; angle += 5)  {
-   servoPulse(servo, angle);  }
-    delay(300);
-    distance_L = Ultrasonic_read();
-    delay(100);
-  for (int angle = 140; angle >= 0; angle -= 5)  {
-   servoPulse(servo, angle);  }
+  //If distance is within 30 cm then adjust motor direction as below
+  if (distance > 0 && distance < DISTANCE_TO_CHECK)
+  {
+    //Stop motors
+    rotateMotor(0, 0);
+    delay(500);  
+       
+    //Reverse motors
+    rotateMotor(-MAX_MOTOR_TURN_SPEED, -MAX_MOTOR_TURN_SPEED);        
+    delay(200);
+    
+    //Stop motors
+    rotateMotor(0, 0);
     delay(500);
-    distance_R = Ultrasonic_read();
-    delay(100);
- for (int angle = 0; angle <= 70; angle += 5)  {
-   servoPulse(servo, angle);  }
-    delay(300);
-    compareDistance();
+    
+    //Rotate servo to left    
+    myServo.write(180);
+    delay(500);
+
+    //Read left side distance using ultrasonic sensor
+    int distanceLeft = mySensor.ping_cm();    
+
+    //Rotate servo to right
+    myServo.write(0);    
+    delay(500);    
+
+    //Read right side distance using ultrasonic sensor   
+    int distanceRight = mySensor.ping_cm();
+
+    //Bring servo to center
+    myServo.write(90); 
+    delay(500);        
+    
+    if (distanceLeft == 0 )
+    {
+      rotateMotor(MAX_MOTOR_TURN_SPEED, -MAX_MOTOR_TURN_SPEED);
+      delay(500);
+    }
+    else if (distanceRight == 0 )
+    {
+      rotateMotor(-MAX_MOTOR_TURN_SPEED, MAX_MOTOR_TURN_SPEED);
+      delay(500);
+    }
+    else if (distanceLeft >= distanceRight)
+    {
+      rotateMotor(MAX_MOTOR_TURN_SPEED, -MAX_MOTOR_TURN_SPEED);
+      delay(500);
+    }
+    else
+    {
+      rotateMotor(-MAX_MOTOR_TURN_SPEED, MAX_MOTOR_TURN_SPEED);
+      delay(500);      
+    }
+    rotateMotor(0, 0);    
+    delay(200);     
+  }
+  else
+  {
+    rotateMotor(MAX_REGULAR_MOTOR_SPEED, MAX_REGULAR_MOTOR_SPEED);
+  }  
 }
 
-void forword(){  //forword
-digitalWrite(in1, HIGH); //Right Motor forword Pin 
-digitalWrite(in2, LOW);  //Right Motor backword Pin 
-digitalWrite(in3, LOW);  //Left Motor backword Pin 
-digitalWrite(in4, HIGH); //Left Motor forword Pin 
+void lineFollowerControl()
+{
+  static int MOTOR_SPEED = 100;
+  static int TURNING_MOTOR_SPEED = 200;
+  
+  int rightIRSensorValue = digitalRead(BOTTOM_IR_SENSOR_RIGHT);
+  int leftIRSensorValue = digitalRead(BOTTOM_IR_SENSOR_LEFT);
+
+  //If none of the sensors detects black line, then go straight
+  if (rightIRSensorValue == LOW && leftIRSensorValue == LOW)
+  {
+    rotateMotor(MOTOR_SPEED, MOTOR_SPEED);
+  }
+  //If right sensor detects black line, then turn right
+  else if (rightIRSensorValue == HIGH && leftIRSensorValue == LOW )
+  {
+      rotateMotor(-TURNING_MOTOR_SPEED, TURNING_MOTOR_SPEED); 
+  }
+  //If left sensor detects black line, then turn left  
+  else if (rightIRSensorValue == LOW && leftIRSensorValue == HIGH )
+  {
+      rotateMotor(TURNING_MOTOR_SPEED, -TURNING_MOTOR_SPEED); 
+  } 
+  //If both the sensors detect black line, then stop 
+  else 
+  {
+    rotateMotor(0, 0);
+  }
 }
 
-void backword(){ //backword
-digitalWrite(in1, LOW);  //Right Motor forword Pin 
-digitalWrite(in2, HIGH); //Right Motor backword Pin 
-digitalWrite(in3, HIGH); //Left Motor backword Pin 
-digitalWrite(in4, LOW);  //Left Motor forword Pin 
+void lineFollowerWithObstacleAvoidingControl()
+{
+  static int MOTOR_SPEED = 100;
+  static int TURNING_MOTOR_SPEED = 200;
+  static int DISTANCE_TO_CHECK  = 20;
+    
+  int rightIRSensorValue = digitalRead(BOTTOM_IR_SENSOR_RIGHT);
+  int leftIRSensorValue = digitalRead(BOTTOM_IR_SENSOR_LEFT);
+
+  //If none of the sensors detects black line, then go straight
+  if (rightIRSensorValue == LOW && leftIRSensorValue == LOW)
+  {
+    int distance = mySensor.ping_cm();
+    //If object detected that is if distance is within 30 cm then adjust motor direction as below
+    if (distance > 0 && distance < DISTANCE_TO_CHECK)
+    {
+      adjustRobotToAvoidObjectWhileFollowingLine();
+    }
+    else
+    {
+      rotateMotor(MOTOR_SPEED, MOTOR_SPEED);
+    }
+  }
+  //If right sensor detects black line, then turn right
+  else if (rightIRSensorValue == HIGH && leftIRSensorValue == LOW )
+  {
+      rotateMotor(-TURNING_MOTOR_SPEED, TURNING_MOTOR_SPEED); 
+  }
+  //If left sensor detects black line, then turn left  
+  else if (rightIRSensorValue == LOW && leftIRSensorValue == HIGH )
+  {
+      rotateMotor(TURNING_MOTOR_SPEED, -TURNING_MOTOR_SPEED); 
+  } 
+  //If both the sensors detect black line, then stop 
+  else 
+  {
+    rotateMotor(0, 0);
+  }  
 }
 
-void turnRight(){ //turnRight
-digitalWrite(in1, LOW);  //Right Motor forword Pin 
-digitalWrite(in2, HIGH); //Right Motor backword Pin  
-digitalWrite(in3, LOW);  //Left Motor backword Pin 
-digitalWrite(in4, HIGH); //Left Motor forword Pin 
+
+void objectFollowingControl()
+{
+  static int MAX_FORWARD_MOTOR_SPEED  = 150;
+  static int MAX_MOTOR_TURN_SPEED_ADJUSTMENT  = 100;
+  static int MIN_DISTANCE  = 10;
+  static int MAX_DISTANCE  = 30;
+  
+  int distance = mySensor.ping_cm();
+  int rightIRSensorValue = digitalRead(TOP_IR_SENSOR_RIGHT);
+  int leftIRSensorValue = digitalRead(TOP_IR_SENSOR_LEFT);
+
+  //NOTE: If IR sensor detects the hand then its value will be LOW else the value will be HIGH
+  
+  //If right sensor detects hand, then turn right. We increase left motor speed and decrease the right motor speed to turn towards right
+  if (rightIRSensorValue == LOW && leftIRSensorValue == HIGH )
+  {
+      rotateMotor(MAX_FORWARD_MOTOR_SPEED - MAX_MOTOR_TURN_SPEED_ADJUSTMENT, MAX_FORWARD_MOTOR_SPEED + MAX_MOTOR_TURN_SPEED_ADJUSTMENT ); 
+  }
+  //If left sensor detects hand, then turn left. We increase right motor speed and decrease the left motor speed to turn towards left
+  else if (rightIRSensorValue == HIGH && leftIRSensorValue == LOW )
+  {
+      rotateMotor(MAX_FORWARD_MOTOR_SPEED + MAX_MOTOR_TURN_SPEED_ADJUSTMENT, MAX_FORWARD_MOTOR_SPEED - MAX_MOTOR_TURN_SPEED_ADJUSTMENT); 
+  }
+  //If distance is between min and max then go straight
+  else if (distance >= MIN_DISTANCE && distance <= MAX_DISTANCE)
+  {
+    rotateMotor(MAX_FORWARD_MOTOR_SPEED, MAX_FORWARD_MOTOR_SPEED);
+  }
+  //stop the motors
+  else 
+  {
+    rotateMotor(0, 0);
+  }  
 }
 
-void turnLeft(){ //turnLeft
-digitalWrite(in1, HIGH); //Right Motor forword Pin 
-digitalWrite(in2, LOW);  //Right Motor backword Pin 
-digitalWrite(in3, HIGH); //Left Motor backword Pin 
-digitalWrite(in4, LOW);  //Left Motor forword Pin 
+void adjustRobotToAvoidObjectWhileFollowingLine()
+{
+  static int MAX_MOTOR_TURN_SPEED  = 200;
+
+  //Stop motors
+  rotateMotor(0, 0);
+  delay(500);
+    
+  //Rotate servo to left    
+  myServo.write(180);
+  delay(500);
+
+  //Read left side distance using ultrasonic sensor
+  int distanceLeft = mySensor.ping_cm();    
+
+  //Rotate servo to right
+  myServo.write(0);    
+  delay(500);    
+
+  //Read right side distance using ultrasonic sensor   
+  int distanceRight = mySensor.ping_cm();
+
+  //Bring servo to center
+  myServo.write(90); 
+  delay(500);        
+
+  bool robotMovedToLeftDirection = false;
+  if (distanceLeft == 0 )
+  {
+    robotMovedToLeftDirection = true;
+    rotateMotor(MAX_MOTOR_TURN_SPEED, -MAX_MOTOR_TURN_SPEED);
+    delay(500);
+  }
+  else if (distanceRight == 0 )
+  {
+    rotateMotor(-MAX_MOTOR_TURN_SPEED, MAX_MOTOR_TURN_SPEED);
+    delay(500);
+  }
+  else if (distanceLeft >= distanceRight)
+  {
+    robotMovedToLeftDirection = true;
+    rotateMotor(MAX_MOTOR_TURN_SPEED, -MAX_MOTOR_TURN_SPEED);
+    delay(500);
+  }
+  else
+  {
+    rotateMotor(-MAX_MOTOR_TURN_SPEED, MAX_MOTOR_TURN_SPEED);
+    delay(500);      
+  }
+  
+  rotateMotor(MAX_MOTOR_TURN_SPEED, MAX_MOTOR_TURN_SPEED);    
+  delay(500); 
+
+  if (robotMovedToLeftDirection)
+  {
+    rotateMotor(-MAX_MOTOR_TURN_SPEED, MAX_MOTOR_TURN_SPEED);
+    delay(800);  
+  }
+  else
+  {
+    rotateMotor(MAX_MOTOR_TURN_SPEED, -MAX_MOTOR_TURN_SPEED);
+    delay(800);  
+  }
 }
 
-void Stop(){ //stop
-digitalWrite(in1, LOW); //Right Motor forword Pin 
-digitalWrite(in2, LOW); //Right Motor backword Pin 
-digitalWrite(in3, LOW); //Left Motor backword Pin 
-digitalWrite(in4, LOW); //Left Motor forword Pin 
+void rotateMotor(int rightMotorSpeed, int leftMotorSpeed)
+{
+  if (rightMotorSpeed < 0)
+  {
+    digitalWrite(rightMotorPin1,LOW);
+    digitalWrite(rightMotorPin2,HIGH);    
+  }
+  else if (rightMotorSpeed >= 0)
+  {
+    digitalWrite(rightMotorPin1,HIGH);
+    digitalWrite(rightMotorPin2,LOW);      
+  }
+
+  if (leftMotorSpeed < 0)
+  {
+    digitalWrite(leftMotorPin1,LOW);
+    digitalWrite(leftMotorPin2,HIGH);    
+  }
+  else if (leftMotorSpeed >= 0)
+  {
+    digitalWrite(leftMotorPin1,HIGH);
+    digitalWrite(leftMotorPin2,LOW);      
+  }
+
+  analogWrite(enableRightMotor, abs(rightMotorSpeed));
+  analogWrite(enableLeftMotor, abs(leftMotorSpeed));    
+}
+
+void loop()
+{
+  switch (ROBOT_CONTROL_MODE)
+  {
+    case OBSTACLE_AVOIDING_MODE:
+      obstacleAvoidingControl();
+      break;
+
+    case LINE_FOLLOWER_MODE:
+      lineFollowerControl();
+      break;
+
+    case LINE_FOLLOWER_WITH_OBSTACLE_AVOIDING_MODE:
+      lineFollowerWithObstacleAvoidingControl();
+      break;
+
+    case OBJECT_FOLLOWING_MODE:
+      objectFollowingControl();
+      break;
+
+    default:
+      break;  
+  }
+ 
 }
